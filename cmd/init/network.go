e5e18227a1949b76a61bc69e7819a70e7cbba597
Brian Goff <cpuguy83@gmail.com>
2023-06-07 00:52:02 +0000

    init: range through links instead of hardcoding name

--- cmd/init/network.go
+++ cmd/init/network.go
@@ -11,20 +11,59 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-const ifaceName = "eth0"
+const l0 = "l0"
 
 func setupNetwork() error {
-	link, err := netlink.LinkByName(ifaceName)
+	links, err := netlink.LinkList()
 	if err != nil {
-		ls, _ := net.Interfaces()
-		ifaces := make([]string, 0, len(ls))
-		for _, l := range ls {
-			ifaces = append(ifaces, l.Name)
-		}
-		return fmt.Errorf("error getting link %q: %w: interfaces: %s", ifaceName, err, strings.Join(ifaces, ", "))
+		return fmt.Errorf("error getting link list: %w", err)
 	}
-	if err := netlink.LinkSetUp(link); err != nil {
-		return fmt.Errorf("error setting link %q to up state: %w", ifaceName, err)
+	for _, link := range links {
+		attrs := link.Attrs()
+		if attrs.Name == l0 {
+			continue
+		}
+		if err := netlink.LinkSetUp(link); err != nil {
+			return fmt.Errorf("error setting link %q to up state: %w", attrs.Name, err)
+		}
+
+		client, err := dhcp.New(attrs.Name)
+		if err != nil {
+			return fmt.Errorf("error creating DHCP client: %w", err)
+		}
+
+		lease, err := client.Request(context.TODO())
+		if err != nil {
+			return fmt.Errorf("error requesting DHCP: %w", err)
+		}
+
+		if err := netlink.AddrAdd(link, &netlink.Addr{
+			IPNet: &net.IPNet{
+				IP:   lease.ACK.YourIPAddr,
+				Mask: lease.ACK.SubnetMask(),
+			},
+			Label:     attrs.Name,
+			Flags:     int(lease.ACK.Flags),
+			Broadcast: lease.ACK.BroadcastAddress(),
+		}); err != nil {
+			return fmt.Errorf("error adding address to link: %w", err)
+		}
+
+		if len(lease.ACK.DNS()) > 0 {
+			b := strings.Builder{}
+			for _, addr := range lease.ACK.DNS() {
+				b.WriteString("nameserver " + addr.String() + "\n")
+			}
+			if err := os.WriteFile("/etc/resolv.conf", []byte(b.String()), 0644); err != nil {
+				return fmt.Errorf("error writing /etc/resolv.conf: %w", err)
+			}
+		}
+		if err := netlink.RouteAdd(&netlink.Route{
+			Gw: lease.ACK.ServerIPAddr,
+		}); err != nil {
+			return fmt.Errorf("error adding route: %w", err)
+		}
+		break
 	}
 
 	lo, err := netlink.LinkByName("lo")
@@ -33,43 +72,6 @@ func setupNetwork() error {
 	}
 	if err := netlink.LinkSetUp(lo); err != nil {
 		return fmt.Errorf("error setting link %q to up state: %w", "lo", err)
-	}
-
-	client, err := dhcp.New(ifaceName)
-	if err != nil {
-		return fmt.Errorf("error creating DHCP client: %w", err)
-	}
-
-	lease, err := client.Request(context.TODO())
-	if err != nil {
-		return fmt.Errorf("error requesting DHCP: %w", err)
-	}
-
-	if err := netlink.AddrAdd(link, &netlink.Addr{
-		IPNet: &net.IPNet{
-			IP:   lease.ACK.YourIPAddr,
-			Mask: lease.ACK.SubnetMask(),
-		},
-		Label:     ifaceName,
-		Flags:     int(lease.ACK.Flags),
-		Broadcast: lease.ACK.BroadcastAddress(),
-	}); err != nil {
-		return fmt.Errorf("error adding address to link: %w", err)
-	}
-
-	if len(lease.ACK.DNS()) > 0 {
-		b := strings.Builder{}
-		for _, addr := range lease.ACK.DNS() {
-			b.WriteString("nameserver " + addr.String() + "\n")
-		}
-		if err := os.WriteFile("/etc/resolv.conf", []byte(b.String()), 0644); err != nil {
-			return fmt.Errorf("error writing /etc/resolv.conf: %w", err)
-		}
-	}
-	if err := netlink.RouteAdd(&netlink.Route{
-		Gw: lease.ACK.ServerIPAddr,
-	}); err != nil {
-		return fmt.Errorf("error adding route: %w", err)
 	}
 
 	return nil
